ec5b31a65c5c144ffef9720ec72c08e1b3e8a523
JA10 <johnantony6780@gmail.com>
2024-12-17 11:20:10 +0530

    new changes

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.movieapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText et1,et2;
+Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        et1=(EditText) findViewById(R.id.unam);
+        et2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.log);
 
     }
 }
