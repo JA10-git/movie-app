6883902b2c68cf22181e360deae38fec086370d3
JA10 <johnantony6780@gmail.com>
2024-12-17 14:02:39 +0530

    add movie pg

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +25,22 @@ Button b1;
         et1=(EditText) findViewById(R.id.unam);
         et2=(EditText) findViewById(R.id.pass);
         b1=(Button) findViewById(R.id.log);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=et1.getText().toString().trim();
+                String s2=et2.getText().toString().trim();
+
+                if (s1.equals("theater") && s2.equals("12345")) {
+                    Intent i = new Intent(getApplicationContext(), menuActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(),"invalid credentials",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/movieapp/menuActivity.java
@@ -0,0 +1,40 @@
+package com.example.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class menuActivity extends AppCompatActivity {
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+        b1=(Button) findViewById(R.id.exit);
+        b2=(Button) findViewById(R.id.add);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1=new Intent(getApplicationContext(), addMovie.class);
+                startActivity(i1);
+            }
+        });
+    }
+}
